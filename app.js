6fbf6b17358042fb8e5ef0b0909319348810958d
Marty Nelson <marty@martypdx.com>
2022-09-21 09:58:58 -0700

    initial fetch of plant data

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 /* Imports */
+import { getPlants } from './fetch-utils.js';
 
 /* Get DOM Elements */
+const notificationDisplay = document.getElementById('notification-display');
 
 /* State */
 let error = null;
 let count = 0;
-
+let plants = [];
 /* Events */
-window.addEventListener('load', async () => {});
+
+// window load event
+window.addEventListener('load', async () => {
+    // call a function and get 100 plants
+    const response = await getPlants();
+
+    // keep in errors
+    error = response.error;
+    // update plants state
+    plants = response.data;
+    console.log(plants);
+
+    displayNotifications();
+    // call displayPlants function
+});
 
 /* Display Functions */
+function displayNotifications() {
+    if (error) {
+        notificationDisplay.classList.add('error');
+        notificationDisplay.textContent = error.message;
+    } else {
+        notificationDisplay.classList.remove('error');
+    }
+}
--- fetch-utils.js
+++ fetch-utils.js
@@ -3,3 +3,8 @@ const SUPABASE_KEY =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl5YWZybnJodmJ2ZWhpZmx0bGtsIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTU5NDA2NDEsImV4cCI6MTk3MTUxNjY0MX0.xMaK7QxF8ut26HwnOeZONCj9728N9XXm0bIknwpAUtg';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+export async function getPlants() {
+    const response = await client.from('plants').select('*').limit(50);
+    return response;
+}
